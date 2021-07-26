7eb99db7ef615f37d5b6a23b38b40998507bad74
Michal Musial <michal.musial554@gmail.com>
2021-07-26 16:33:22 +0200

    portfolio layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box } from '@material-ui/core';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  topBar: {
+    height: '64px',
+    padding: '0% 15% 0% 15%;',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    background: '#2C2E43'
+  },
+  h1: {
+    margin: '0',
+    marginLeft: '10px',
+    color: 'white',
+    fontWeight: 'bold',
+    fontFamily: 'Gloria'
+  },
+  divider1: {
+    height: '100vh',
+    background: '#595260',
+  },
+  divider2: {
+    height: '75vh',
+    background: '#FFD523',
+  }
+}));
+
+const App = ({ }) => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+    <div className={classes.divider1}> 
     </div>
-  );
-}
+    <div className={classes.topBar}>
+        <h1 className={classes.h1}>Musialowski</h1>
+        <Box display='flex'>
+          <h2 className={classes.h1}>Navi</h2>
+          <h2 className={classes.h1}>Navi</h2>
+          <h2 className={classes.h1}>Navi</h2>
+        </Box>
+    </div>
+    <div className={classes.divider2}>
+
+    </div>
+  </>
+  )
+};
 
 export default App;
